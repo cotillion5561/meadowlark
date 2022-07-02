c893d04fcd7024bb8cad031b7a63a029f95794e9
cotillion5561 <cotillion5561@gmail.com>
2022-07-02 13:51:31 +0300

    newsletter fetch, json service added

--- meadowlark.js
+++ meadowlark.js
@@ -1,62 +1,75 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const fortune = require('./lib/fortune')
-const handlers = require('./lib/handlers')
-const weatherMiddleware = require('./lib/middleware/weather')
 const bodyParser = require('body-parser')
 //const multiparty = require('multiparty')
-const app = express()
 
+const handlers = require('./lib/handlers')
+const weatherMiddlware = require('./lib/middleware/weather')
+
+const app = express();
+
+// configure Handlebars view engine
 app.engine('handlebars', expressHandlebars.engine({
-    defaultLayout: 'main',
-    helpers: {
-        section: function(name, options) {
-          if(!this._sections) this._sections = {}
-          this._sections[name] = options.fn(this)
-          return null
-        },
-      }
+  defaultLayout: 'main',
+  helpers: {
+    section: function(name, options) {
+      if(!this._sections) this._sections = {}
+      this._sections[name] = options.fn(this)
+      return null
+    },
+  }
 }))
-
 app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
-//app.use(bodyParser.json())
-
+app.use(bodyParser.json())
 
 const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
-app.use(weatherMiddleware)
+app.use(weatherMiddlware)
 
-app.get('/',handlers.home)
+app.get('/', handlers.home)
 
-app.get('/about',handlers.about)
-
-app.get('/headers', (req, res) => {
-    res.type('text/plain')
-    const headers = Object.entries(req.headers)
-        .map(([key, value]) => `${key} : ${value}`)
-    res.send(headers.join('\n'))
-})
-
+// handlers for browser-based form submission
 app.get('/newsletter-signup', handlers.newsletterSignup)
 app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
 app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
 
-//custom 404 page
-app.use(handlers.notFound)
+// handlers for fetch/JSON form submission
+app.get('/newsletter', handlers.newsletter)
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
 
-//custom 500 page
+/* // vacation photo contest
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest)
+app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax)
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+  const form = new multiparty.Form()
+  form.parse(req, (err, fields, files) => {
+    if(err) return handlers.vacationPhotoContestProcessError(req, res, err.message)
+    console.log('got fields: ', fields)
+    console.log('and files: ', files)
+    handlers.vacationPhotoContestProcess(req, res, fields, files)
+  })
+}) 
+app.get('/contest/vacation-photo-thank-you', handlers.vacationPhotoContestProcessThankYou)
+app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
+  const form = new multiparty.Form()
+  form.parse(req, (err, fields, files) => {
+    if(err) return handlers.api.vacationPhotoContestError(req, res, err.message)
+    handlers.api.vacationPhotoContest(req, res, fields, files)
+  })
+})
+*/
+app.use(handlers.notFound)
 app.use(handlers.serverError)
 
-
-if(require.main === module){
-    app.listen(port, () => {
-        console.log(`Express started on http://localhost:${port}  ` +
-        ` press Ctrl + C to terminate`)
-    })
-}else{
-    module.exports = app
+if(require.main === module) {
+  app.listen(port, () => {
+    console.log( `Express started on http://localhost:${port}` +
+      '; press Ctrl-C to terminate.' )
+  })
+} else {
+  module.exports = app
 }
